400f7246daf827f9c027c2ce48e23ceb15ffa70e
Mwaniki Njagi <46248187+MwanikiNjagi@users.noreply.github.com>
2022-05-24 10:32:57 +0300

    Add files via upload

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,10 +7,12 @@ def main():
     fill_vals(df)
     availabilty(df)
     #society_encoded(df)
+    area_type_encoded(df)
     df.to_csv("./Input/preprocessed_data.csv")
-    #print(df.head(20))
+    df = df.drop(["area_type", "availability", "bath", "balcony"], axis=1)
+    print(df.head(20))
     #print("Data has been preprocessed and converted to a csv file")
-    print (df["society"].value_counts())
+    #print (df["society"].value_counts())
 
 
 #Fills NaN values in balcony and bath and drops all rows with NaN values in the society column
@@ -19,6 +21,8 @@ def fill_vals(df, column_1 = "balcony", column_2 = "bath"):
     df[column_2].fillna(method='ffill', inplace=True)
     df[column_1] = df[column_1].astype(int)
     df[column_2] = df[column_2].astype(int)
+    df["bath_processed"] =  df[column_2]
+    df["balcony_processed"] = df[column_1]
     df.dropna(subset=["society"], inplace=True)
     return df
 
@@ -30,11 +34,17 @@ def availabilty(df, column = "availability"):
     df[column] = df[column].apply(lambda x:x.split("-")[0])
     df[column] = df[column].astype(int)
     df.loc[df[column]>0, column] = 2
+    df["availability_encoded"] =  df[column]
     return df
 
 #def society_encoded(df, column="society"):
-       
 
+def area_type_encoded(df, column = "area_type"):  
+    LE = LabelEncoder()
+    df["area_type_encoded"] = LE.fit_transform(df[column])
+    return df     
+
+#def 
 if __name__ == "__main__":
     main()
    
